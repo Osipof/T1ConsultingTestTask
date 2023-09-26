25a49c82438b22a451842d503f1704622601becf
Ivan Osipov <iv.anek@mail.ru>
2023-09-26 16:25:15 +0300

    Add spring boot

---
+++ src/main/java/com/example/T1ConsultingTestTask/T1ConsultingTestTaskApplication.java
@@ -0,0 +1,13 @@
+package com.example.T1ConsultingTestTask;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class T1ConsultingTestTaskApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(T1ConsultingTestTaskApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/example/T1ConsultingTestTask/T1ConsultingTestTaskApplicationTests.java
@@ -0,0 +1,13 @@
+package com.example.T1ConsultingTestTask;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class T1ConsultingTestTaskApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
